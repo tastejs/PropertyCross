0148c955f3f1788ee6fe1fd491a506cbbf83498a
Chris Price <price.c@gmail.com>
2012-09-24 14:52:55 +0100

    added tap to load more button

--- Titanium/Resources/view/SearchResultsView.js
+++ Titanium/Resources/view/SearchResultsView.js
@@ -1,63 +1,69 @@
 var _ = require("underscore");
 var ko = require("knockout");
 
+function createRow(imageUrl, title, description) {
+	var row = Ti.UI.createTableViewRow();
+	row.height = '82dip';
+	// tip the abstraction off that the rows have the same layout
+	row.className = 'myrows';
+	// use an image view rather than a plain view
+	row.add(Ti.UI.createImageView({
+		url : imageUrl,
+		top : '11dip',
+		left : '6dip',
+		width : '80dip',
+		height : '60dip'
+	}));
+	row.add(Titanium.UI.createLabel({
+		text : title,
+		color : '#2F3E46',
+		textAlign : 'left',
+		font : {
+			fontSize : '16dip',
+			fontWeight : 'bold'
+		},
+		top : '21dip',
+		left : '100dip'
+	}));
+	row.add(Titanium.UI.createLabel({
+		text : description,
+		color : '#2F3E46',
+		textAlign : 'left',
+		font : {
+			fontSize : '12dip',
+			fontWeight : 'bold'
+		},
+		top : '46dip',
+		left : '100dip'
+	}));
+	return row;
+}
+
 module.exports = function(viewModel) {
 
 	var window = Ti.UI.createWindow();
-	var tableView;
-
-	//
-	// create table view (
-	//
-	tableView = Titanium.UI.createTableView({
+	var tableView = Titanium.UI.createTableView({
 		backgroundColor : 'white'
 	});
 	tableView.addEventListener('click', function(e) {
-		viewModel.properties()[e.index].select();
+		if (e.index === viewModel.properties().length) {
+			viewModel.loadMore();
+		} else {
+			viewModel.properties()[e.index].select();
+		}
 	});
 
 	window.add(tableView);
 	this.window = window;
 
 	function updateRows(properties) {
-		tableView.setData(_.map(properties, function(property) {
-			// create first row
-			var row = Ti.UI.createTableViewRow();
-			row.height = '82dip';
-			// tip the abstraction off that the rows have the same layout
-			row.className = 'myrows';
-			// use an image view rather than a plain view
-			row.add(Ti.UI.createImageView({
-				url : property.thumbnailUrl,
-				top : '11dip',
-				left : '6dip',
-				width : '80dip',
-				height : '60dip'
-			}));
-			row.add(Titanium.UI.createLabel({
-				text : '£ ' + property.price,
-				color : '#2F3E46',
-				textAlign : 'left',
-				font : {
-					fontSize : '16dip',
-					fontWeight : 'bold'
-				},
-				top : '21dip',
-				left : '100dip'
-			}));
-			row.add(Titanium.UI.createLabel({
-				text : property.title + ' ' + property.bedrooms + ' bed ' + property.propertyType,
-				color : '#2F3E46',
-				textAlign : 'left',
-				font : {
-					fontSize : '12dip',
-					fontWeight : 'bold'
-				},
-				top : '46dip',
-				left : '100dip'
-			}));
-			return row;
-		}));
+		var rows = _.map(properties, function(property) {
+			return createRow(property.thumbnailUrl, '£ ' + property.price, property.title + ' ' + property.bedrooms + ' bed ' + property.propertyType);
+		});
+		if (viewModel.properties().length < viewModel.totalResults) {
+			rows.push(createRow("/pull-icon.png", "Tap to load more...", "Results for " + viewModel.searchLocation.displayString + ", showing " + viewModel.properties().length + " of " + viewModel.totalResults + " properties"));
+		}
+		tableView.setData(rows);
 	}
 
 	var propertiesSubscription = viewModel.properties.subscribe(function(properties) {
