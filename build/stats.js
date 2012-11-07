58df78f7603e767cd152e24bf89a76f1dc6896fe
Chris Price <price.c@gmail.com>
2012-11-07 12:49:16 +0000

    add stats generation - basic fs recursion

---
+++ build/stats.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+var path = require('path');
+
+find('.', function(item, itemPath) {
+      console.log(item, itemPath);
+      return true;
+    },
+    function(allPaths) {
+      console.log(allPaths);
+    });
+
+function find(root, filter, callback) {
+  function innerFind(root) {
+    return fs.readdirSync(root).map(function (item) {
+      var itemPath = path.join(root, item);
+      var stats = fs.statSync(itemPath);
+      if (!filter(item, itemPath, stats)) {
+        return;
+      }
+      if (stats.isDirectory()) {
+        return innerFind(itemPath);
+      } else {
+        return itemPath;
+      }
+    }).reduce(function (all, files) {
+          return files ? all.concat(files) : all;
+        }, []);
+
+  }
+  callback(innerFind(root));
+}
