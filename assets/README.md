07408a7caafb344e1ab0fd9dbc9de7607a53da40
Chris Price <price.c@gmail.com>
2012-11-13 17:06:10 +0000

    add asset generation script and configure for jqm android

--- assets/README.md
+++ assets/README.md
@@ -1,21 +1,12 @@
 # Assets
 
-This folder contains a number of assets that are used across the various implementations
+This folder contains a script to generate assets that are used across the various implementations.
 
-## Icons
+[Node](http://nodejs.org), [npm](https://npmjs.org/) and [ImageMagick](http://www.imagemagick.org/script/binary-releases.php) are pre-requisites.
 
-  * `Icon66x66.png` - Windows Phone small app icon
-  * `Icon173x173.png` - Windows Phone large app icon
-  * `Icon.png` - iPhone icon
-  * `Icon@2x.png` - iPhone retina icon
-  * `Icon96x96.png` - Android xhdpi  icon
-  * `Icon72x72.png` - Android hdpi  icon
-  * `Icon48x48.png` - Android mdpi  icon
-  * `Icon36x36.png` - Android ldpi  icon
+To run the script -
+```
+node generate-images
+```
 
-## Splashscreens
-   
-  * `SplashScreenImage.jpg` - Windows Phone splashscreen
-  * `Default.png` - iPhone splashscreen
-  * `Default@2x.png` - iPhone retina splashscreen
-  * `Default-568h@2x.png` - iPhone 5 retina splashscreen
+To generate additional assets modify the arrays at the top of the script.
---
+++ assets/generate-images.js
@@ -0,0 +1,67 @@
+generateIcons("icon-base-173x173.png", [
+// ["FRAMEWORK-OVERLAY", "TARGET", WIDTH],
+  ["frameworks/jquerymobile.png", "../jquerymobile/android/assets/icons/36x36.png", 36],
+  ["frameworks/jquerymobile.png", "../jquerymobile/android/assets/icons/48x48.png", 48],
+  ["frameworks/jquerymobile.png", "../jquerymobile/android/assets/icons/72x72.png", 72],
+  ["frameworks/jquerymobile.png", "../jquerymobile/android/assets/icons/96x96.png", 96]
+]);
+
+generateSplashscreens("splashscreen-bottom-640x640.png", [
+// ["TARGET", WIDTH, HEIGHT],
+  ["../jquerymobile/android/assets/splashscreens/200x320.png", 200, 320],
+  ["../jquerymobile/android/assets/splashscreens/320x480.png", 320, 480],
+  ["../jquerymobile/android/assets/splashscreens/480x800.png", 480, 800],
+  ["../jquerymobile/android/assets/splashscreens/720x1280.png", 720, 1280]
+]);
+
+
+function template(tmpl, values) {
+  return tmpl.replace(/\{([^\}]*)\}/g, function(brackets, key) {
+    return values[key] || '';
+  });
+}
+
+function exec(tmpl, config, callback) {
+  var cmd = template(tmpl, config);
+  console.log(cmd);
+  return require('child_process').exec(cmd, function(err) {
+    if (callback) {
+      return callback(err);
+    }
+  });
+}
+
+function generateIcons(background, icons) {
+  icons.forEach(function(config) {
+    exec("convert {background} {overlay} -composite -resize {width}x{height}! {result}", {
+      background: background,
+      overlay: config[0],
+      width: config[2],
+      height: config[2],
+      result: config[1]
+    }, function(err, sdout, sderr) {
+      if (err) {
+        console.error(err);
+      }
+    });
+  });
+}
+function generateSplashscreens(source, splashscreens) {
+  splashscreens.forEach(function(config) {
+    var width = config[1], height = config[2];
+    var nominalHeight = height / width * 640;
+    exec("convert -size {nominalWidth}x{nominalHeight} canvas:black {source} -geometry +0+{offset} -composite -resize {width}x{height}! {result}", {
+      nominalWidth: 640,
+      nominalHeight: nominalHeight,
+      offset: nominalHeight - 640,
+      source: source,
+      width: width,
+      height: height,
+      result: config[0]
+    }, function(err, sdout, sderr) {
+      if (err) {
+        console.error(err);
+      }
+    });
+  });
+}
