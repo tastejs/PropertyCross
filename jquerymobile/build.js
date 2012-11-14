cae040c794d24b107f5876f347e4354bfd92c1c3
Chris Price <price.c@gmail.com>
2012-11-14 16:41:46 +0000

    add common code diff as part of the build script (this spotted the code wasn't in sync so copied property.js over)

--- jquerymobile/build.js
+++ jquerymobile/build.js
@@ -1,3 +1,21 @@
+// ensure all the model and viewModel code is in sync
+var fs = require('fs');
+var find = require('../build/find');
+var platforms = ['android', 'ios', 'windowsphone'];
+find('common', '*.js', function(paths) {
+  paths.forEach(function(path) {
+    var commonCode = fs.readFileSync(path);
+    platforms.forEach(function(platform) {
+      var platformPath = path.replace('common', platform);
+      var platformCode = fs.readFileSync(platformPath);
+      if (commonCode.toString() != platformCode.toString()) {
+        throw path + " does not match " + platformPath;
+      }
+    });
+  });
+});
+
+// run platform builds
 require('./android/build');
 require('./ios/build');
 require('./windowsphone/build');
