1cfd671690c11d65eeb9c6076500378fccc1baf1
vinothini balakrishnan <vinothinibj@gmail.com>
2013-01-07 17:25:09 +0530

    Instead of alert showing error message in text format

--- app_in_rhodes/public/js/propertycross.js
+++ app_in_rhodes/public/js/propertycross.js
@@ -1,9 +1,14 @@
+function error_message(msg) {
+	$("#Searching_label").hide();
+	$("#error_message").html(msg);
+}
+
 $(document).ready(function() {
 
 	$("#go").live('click', function() {
 			var search_text = $("#search_field").val().trim();
 			if (search_text.length == 0) {
-				alert("Search field should not be empty");
+				error_message("Search field should not be empty");
 			} else {
 			}
 
