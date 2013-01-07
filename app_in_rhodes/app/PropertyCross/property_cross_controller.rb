641378d6213943e2e447fe0109930908c2d63a09
vinothini balakrishnan <vinothinibj@gmail.com>
2013-01-07 17:52:35 +0530

    Get application_response_code from response

--- app_in_rhodes/app/PropertyCross/property_cross_controller.rb
+++ app_in_rhodes/app/PropertyCross/property_cross_controller.rb
@@ -8,6 +8,9 @@ class PropertyCrossController < Rho::RhoController
   def search_listings
     if has_network?
       place_name = @params['place_name']
+      url =  "http://api.nestoria.co.uk/api?country=uk&pretty=1&action=search_listings&place_name=#{place_name}&page=1&encoding=json&listing_type=buy"
+      result = Rho::AsyncHttp.get(:url => url)
+      application_response_code = result["body"]["response"]["application_response_code"]
     else
       WebView.execute_js("error_message('An error occurred while searching. Please check your network connection and try again.');")
     end
