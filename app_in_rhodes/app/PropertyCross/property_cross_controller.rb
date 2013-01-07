9d9027691390e99751e18ac5c54be87877687a4b
vinothini balakrishnan <vinothinibj@gmail.com>
2013-01-07 17:53:32 +0530

    If status is ok decide_redirection

--- app_in_rhodes/app/PropertyCross/property_cross_controller.rb
+++ app_in_rhodes/app/PropertyCross/property_cross_controller.rb
@@ -11,6 +11,9 @@ class PropertyCrossController < Rho::RhoController
       url =  "http://api.nestoria.co.uk/api?country=uk&pretty=1&action=search_listings&place_name=#{place_name}&page=1&encoding=json&listing_type=buy"
       result = Rho::AsyncHttp.get(:url => url)
       application_response_code = result["body"]["response"]["application_response_code"]
+      if result['status'] == "ok"
+        decide_redirection(application_response_code, result, place_name)
+      end
     else
       WebView.execute_js("error_message('An error occurred while searching. Please check your network connection and try again.');")
     end
