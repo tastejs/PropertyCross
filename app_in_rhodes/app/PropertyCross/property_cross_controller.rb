8362e05686c1128369b583aba0c21b9608b764f6
vinothini balakrishnan <vinothinibj@gmail.com>
2013-01-07 17:43:27 +0530

    If there is no network aler user by showing check your network connection and try again message

--- app_in_rhodes/app/PropertyCross/property_cross_controller.rb
+++ app_in_rhodes/app/PropertyCross/property_cross_controller.rb
@@ -5,6 +5,12 @@ require 'helpers/application_helper'
 class PropertyCrossController < Rho::RhoController
   include BrowserHelper
   include ApplicationHelper
+  def search_listings
+    if has_network?
+      place_name = @params['place_name']
+    else
+      WebView.execute_js("error_message('An error occurred while searching. Please check your network connection and try again.');")
+    end
+  end
 
-  
 end
