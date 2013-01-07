41102fa0b7ba19bc22d3abc42ddc4a9f1572ee02
vinothini balakrishnan <vinothinibj@gmail.com>
2013-01-07 17:38:01 +0530

    Adding Application helper

--- app_in_rhodes/app/PropertyCross/property_cross_controller.rb
+++ app_in_rhodes/app/PropertyCross/property_cross_controller.rb
@@ -1,8 +1,10 @@
 require 'rho/rhocontroller'
 require 'helpers/browser_helper'
+require 'helpers/application_helper'
 
 class PropertyCrossController < Rho::RhoController
   include BrowserHelper
+  include ApplicationHelper
 
   
 end
