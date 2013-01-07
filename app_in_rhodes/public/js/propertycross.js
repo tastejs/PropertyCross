7eef32a0a36c5422f279055bd53c29610cb5d6a8
vinothini balakrishnan <vinothinibj@gmail.com>
2013-01-07 17:27:00 +0530

    For valid search string making ajax call

--- app_in_rhodes/public/js/propertycross.js
+++ app_in_rhodes/public/js/propertycross.js
@@ -10,6 +10,11 @@ $(document).ready(function() {
 			if (search_text.length == 0) {
 				error_message("Search field should not be empty");
 			} else {
+				$("#Searching_label").show();
+				$("#error_message").html("");
+				var jqxhr = $.post("/app/PropertyCross/search_listings", {
+						"place_name": search_text
+					}, function(data) { });
 			}
 
 		});
