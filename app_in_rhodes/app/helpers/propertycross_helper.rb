937b55a1da0c5bff1548943ff55a7fb10987517f
vinothini balakrishnan <vinothinibj@gmail.com>
2013-01-08 11:00:04 +0530

    Design recent search li

--- app_in_rhodes/app/helpers/propertycross_helper.rb
+++ app_in_rhodes/app/helpers/propertycross_helper.rb
@@ -15,8 +15,20 @@ module PropertycrossHelper
 
   def recent_search(recent_search_result)
     recent_search_info = "<div> <h4> Recent Searches </h4></div>"
-    recent_search_info+="<ul data-role=listview data-inset=true style=margin:0px; ></ul>"
+    recent_search_info+="<ul data-role=listview data-inset=true style=margin:0px; >"+recent_search_li_formation(recent_search_result)+"</ul>"
     recent_search_info
+  end
+
+  def recent_search_li_formation(recent_search_result)
+    li_formation = ""
+    recent_search_result.each do |recent_search_result|
+      if recent_search_result.place_name.to_i == 0
+        li_formation+="<li data-theme=c class=recent_search_li><a href=#>"+recent_search_result.place_name+"</a><span class=misspelt_place_name>"+recent_search_result.place_name+"</span><span class=ui-li-count>"+recent_search_result.count+"</span></li>"
+      else
+        li_formation+="<li data-theme=c class=recent_search_li><a href=#>"+recent_search_result.place_name+"</a><span class=misspelt_place_name id=recent_search_by_my_location>"+recent_search_result.place_name+"</span><span class=ui-li-count>"+recent_search_result.count+"</span></li>"
+      end
+    end
+    li_formation
   end
 
 end
