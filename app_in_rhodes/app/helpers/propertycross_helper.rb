d0283235d342d0122f4bc05dcb9abdba8094f5b6
vinothini balakrishnan <vinothinibj@gmail.com>
2013-01-08 10:35:24 +0530

    Design misspelt location ul

--- app_in_rhodes/app/helpers/propertycross_helper.rb
+++ app_in_rhodes/app/helpers/propertycross_helper.rb
@@ -1,3 +1,8 @@
 module PropertycrossHelper
+  def misspelt_location(locations)
+    missplet_location_info = "<div><h4>Please select a location below:</h4></div>"
+    missplet_location_info+="<ul data-role=listview data-inset=true style=margin:0px; ></ul>"
+    missplet_location_info
+  end
 
 end
