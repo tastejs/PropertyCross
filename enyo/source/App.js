1e58d5537e2574b97bc33dcf06fc00d6cc7dd6bb
dragongears <binky1802>
2013-02-12 15:55:39 -0800

    Enyo framework initial commit with Enyo template app (https://github.com/enyojs/bootplate) as a starting point to creating PropertyCross

---
+++ enyo/source/App.js
@@ -0,0 +1,17 @@
+enyo.kind({
+	name: "App",
+	kind: "FittableRows",
+	fit: true,
+	components:[
+		{kind: "onyx.Toolbar", content: "Hello World"},
+		{kind: "enyo.Scroller", fit: true, components: [
+			{name: "main", classes: "nice-padding", allowHtml: true}
+		]},
+		{kind: "onyx.Toolbar", components: [
+			{kind: "onyx.Button", content: "Tap me", ontap: "helloWorldTap"}
+		]}
+	],
+	helloWorldTap: function(inSender, inEvent) {
+		this.$.main.addContent("The button was tapped.<br/>");
+	}
+});
---
+++ enyo/source/package.js
@@ -0,0 +1,7 @@
+enyo.depends(
+	"$lib/layout",
+	"$lib/onyx",	// To theme Onyx using Theme.less, change this line to $lib/onyx/source,
+	//"Theme.less",	// uncomment this line, and follow the steps described in Theme.less
+	"App.css",
+	"App.js"
+);
