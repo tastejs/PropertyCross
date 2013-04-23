2cbd980a2de2c0504c711001a1169c4abe9b3684
Joel Smith <jsmith@scottlogic.co.uk>
2013-04-23 13:52:50 +0100

    improving faves data binding

--- kendoUI/viewModel/PropertyViewModel.js
+++ kendoUI/viewModel/PropertyViewModel.js
@@ -4,19 +4,24 @@ function PropertyViewModel() {
 
     this.property = {};
     this.fave = false;
-    this.notFave = true //I can't believe this is needed, but can't see a way round it!
 
     this.init = function(property) {
         this.set("property", property);
         var isFave = favouritesService.isFave(this.property);
         this.set("fave", isFave);
-        this.set("notFave", !isFave);
     };
 
     this.addRemoveFave = function(event) {
         var isFave = favouritesService.addRemoveFave(that.property);
         this.set("fave", isFave);
-        this.set("notFave", !isFave);
+    };
+
+    this.isFave = function() {
+        return this.get("fave");
+    };
+
+    this.notFave = function() {
+        return !this.get("fave");
     };
 
     that = kendo.observable(this);
