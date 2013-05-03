0a183b4f78e3b940c14b58b90db196ed8874e826
emmerich <shne24@gmail.com>
2013-05-03 10:22:28 +0100

    Added rendering of properties that were returned from the search.

--- lungo/viewModels/PropertyViewModel.js
+++ lungo/viewModels/PropertyViewModel.js
@@ -1,7 +1,7 @@
 /**
  * Created with JetBrains WebStorm.
  * User: shall
- * Date: 02/05/13
- * Time: 16:46
+ * Date: 03/05/13
+ * Time: 09:40
  * To change this template use File | Settings | File Templates.
  */
---
+++ lungo/viewModels/SearchViewModel.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: shall
+ * Date: 03/05/13
+ * Time: 09:03
+ * To change this template use File | Settings | File Templates.
+ */
