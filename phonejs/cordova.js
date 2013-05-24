5a6fac4b44fa5a2147a7ee9bc5d157179a5d2d22
Aleksey Martynov <bclexa@gmail.com>
2013-05-24 12:22:12 +0400

    PhoneJS tidyup, and #136 bugfix
    Fixed #136 in PhoneJS impl
    
    README excluded from cordova package
    
    generated file added to gitignore
    
    PhoneJS package-exclude cleanup
    
    Grammar fixed in comment
    
    Added minified globalize.js
    
    Removed non-required PhoneJS files

--- phonejs/cordova.js
+++ phonejs/cordova.js
@@ -1,1 +1,1 @@
-// This fill will be added by the native app packager (PhoneGap build, etc)
+// This file is replaced by the native app packager
--- phonejs/layouts/Empty/EmptyLayout.js
+++
@@ -1,9 +0,0 @@
-﻿(function($, DX, undefined) {
-
-    DX.framework.html.EmptyLayoutController = DX.framework.html.DefaultLayoutController.inherit({
-      
-    });
-
-    DX.framework.html.layoutControllers.empty = new DX.framework.html.EmptyLayoutController();
-    
-})(jQuery, DevExpress);
