73f7ae7bd76d6d228854a0e91bd351fc9a758a06
James Phillpotts <jphillpotts@scottlogic.co.uk>
2014-01-17 17:00:11 +0000

    Enyo grunt build

---
+++ enyo/Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  var config = {
+    pkg: grunt.file.readJSON("package.json"),
+    execute: {
+      enyo: {
+        src: ['src/enyo/tools/deploy.js'],
+        options: {
+          cwd: 'src'
+        }
+      }
+    }
+  };
+
+  grunt.loadNpmTasks("grunt-execute");
+
+  require("../phonegap/common-with-winphone").build(grunt, config, function(config) {
+    config.defaultTask.splice(1, 0, "execute");
+    config.clean.push("src/build/**");
+    config.clean.push("src/deploy/**");
+    config.compress.main.files.push(
+      { expand: true, src: ["**/*"], cwd: "src/deploy/src", dest: "www" }
+    );
+  });
+
+};
