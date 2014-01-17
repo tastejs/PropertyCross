724c6f694daf7dd031d435c0ea60c56de8411dbb
James Phillpotts <jphillpotts@scottlogic.co.uk>
2014-01-17 12:54:48 +0000

    Grunt build for phonejs

--- phonejs/create-package.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-rem download zip.exe from http://gnuwin32.sourceforge.net/packages/zip.htm
-
-rem del package.zip
-"C:\Program Files (x86)\GnuWin32\bin\zip" -r package.zip * -x@package-exclude.txt 
