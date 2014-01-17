cb1d3068128f28953e2581029876b6568178960f
James Phillpotts <jphillpotts@scottlogic.co.uk>
2014-01-17 12:44:59 +0000

    Grunt build for senchatouch2

---
+++ senchatouch2/Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  var config = {
+    pkg: grunt.file.readJSON("package.json"),
+    exec: {
+      sencha: {
+        cmd: "sencha app build --environment package",
+        cwd: "src"
+      }
+    }
+  };
+
+  grunt.loadNpmTasks("grunt-exec");
+
+  require("../phonegap/common-with-winphone").build(grunt, config, function(config) {
+    config.defaultTask.splice(1, 0, "exec");
+    config.clean.push("src/build/**")
+    config.compress.main.files.push(
+      { expand: true, src: ["**/*"], cwd: "src/build/package/PropertyFinder", dest: "www" }
+    );
+  });
+
+};
---
+++ senchatouch2/src/app.js
@@ -0,0 +1,61 @@
+/*
+    This file is generated and updated by Sencha Cmd. You can edit this file as
+    needed for your application, but these edits will have to be merged by
+    Sencha Cmd when it performs code generation tasks such as generating new
+    models, controllers or views and when running "sencha app upgrade".
+
+    Ideally changes to this file would be limited and most work would be done
+    in other places (such as Controllers). If Sencha Cmd cannot merge your
+    changes and its generated code, it will produce a "merge conflict" that you
+    will need to resolve manually.
+*/
+
+Ext.application({
+    name: 'PropertyFinder',
+
+    requires: [
+        'Ext.MessageBox'
+    ],
+
+    views: [
+        'Main'
+    ],
+
+    icon: {
+        '57': 'resources/icons/Icon.png',
+        '72': 'resources/icons/Icon~ipad.png',
+        '114': 'resources/icons/Icon@2x.png',
+        '144': 'resources/icons/Icon~ipad@2x.png'
+    },
+
+    isIconPrecomposed: true,
+
+    startupImage: {
+        '320x460': 'resources/startup/320x460.jpg',
+        '640x920': 'resources/startup/640x920.png',
+        '768x1004': 'resources/startup/768x1004.png',
+        '748x1024': 'resources/startup/748x1024.png',
+        '1536x2008': 'resources/startup/1536x2008.png',
+        '1496x2048': 'resources/startup/1496x2048.png'
+    },
+
+    launch: function() {
+        // Destroy the #appLoadingIndicator element
+        Ext.fly('appLoadingIndicator').destroy();
+
+        // Initialize the main view
+        Ext.Viewport.add(Ext.create('PropertyFinder.view.Main'));
+    },
+
+    onUpdated: function() {
+        Ext.Msg.confirm(
+            "Application Update",
+            "This application has just successfully been updated to the latest version. Reload now?",
+            function(buttonId) {
+                if (buttonId === 'yes') {
+                    window.location.reload();
+                }
+            }
+        );
+    }
+});
