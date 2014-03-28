fb85dbcae88b86552f64f7e1537130f9cc6254c3
Matthew Dunsdon <matthewdunsdon@gmail.com>
2014-03-28 17:05:28 +0000

    Updated Titanium to use Action Bar in Android
    As part of moving to Android 4.0 (v14) devices no longer needed to provide
    menu buttons.  The design language implies that the Action bar should be
    used to provide the 'favourite' navigation / setting functionality - which
    is how other PropertyCross implementations has solved thiS.

--- titanium/Resources/view/AndroidApplicationView.js
+++ titanium/Resources/view/AndroidApplicationView.js
@@ -2,22 +2,58 @@ var _ = require("lib/underscore");
 var ko = require("lib/knockout");
 var AbstractApplicationView = require("view/AbstractApplicationView");
 var PropertyViewModel = require("viewModel/PropertyViewModel");
+var PropertySearchViewModel = require("viewModel/PropertySearchViewModel");
 
 function AndroidApplicationView(applicationViewModel) {
   AbstractApplicationView.call(this, applicationViewModel);
 
   var that = this;
   var nav;
+  
+  function navBackwards() {
+    if (applicationViewModel.backButtonRequired()) {
+      applicationViewModel.back();
+    } else {
+      Titanium.Android.currentActivity.finish();
+    }
+  }
+  function updateFavouriteMenuItem(menuItem, isFavourite) {
+    menuItem.icon = isFavourite ? 'images/star.png' : 'images/nostar.png';
+    menuItem.title = isFavourite ? "Remove from Favourites" : "Add to Favourites";
+  }
 
   this.navigateForwards = function (viewModel, view) {
+  	  	var win = view.window;
+  	win.addEventListener("open", function(evt) {
+      var actionBar = win.activity.actionBar;
+  	  actionBar.displayHomeAsUp = true;
+  	  actionBar.onHomeIconItemSelected = navBackwards;
+	  win.activity.onCreateOptionsMenu = function (e) {
+	    var menu = e.menu;
+	    var viewModel = applicationViewModel.currentViewModel();
+	    if(viewModel instanceof PropertyViewModel) {
+		    var menuItem = menu.add({ showAsAction: Ti.Android.SHOW_AS_ACTION_ALWAYS });
+		    updateFavouriteMenuItem(menuItem, viewModel.isFavourite());
+		    menuItem.addEventListener("click", function (e) {
+		      var viewModel = applicationViewModel.currentViewModel();
+		      viewModel.addToFavourites();
+		      updateFavouriteMenuItem(menuItem, viewModel.isFavourite());
+		      //menuItem.icon = viewModel.isFavourite()  ? 'images/star.png' : 'images/nostar.png';
+		      //win.activity.invalidateOptionsMenu();
+		    });
+	    } else if (viewModel instanceof PropertySearchViewModel) {
+		  var menuItem = menu.add({
+		    title:"Favourites",
+		    showAsAction: Ti.Android.SHOW_AS_ACTION_ALWAYS | Ti.Android.SHOW_AS_ACTION_WITH_TEXT
+		  });
+		  menuItem.addEventListener("click", function (e) {
+		    applicationViewModel.navigateToFavourites();
+		  });
+	    }
+	  };
+  	});
     // pressing back on the last screen should exit the application
-    view.window.addEventListener('android:back', function () {
-      if (applicationViewModel.backButtonRequired()) {
-        applicationViewModel.back();
-      } else {
-        Titanium.Android.currentActivity.finish();
-      }
-    });
+    view.window.addEventListener('android:back', navBackwards);
     // hide the previous view
     var previousView = _.last(that.viewStack);
     if (previousView) {
@@ -34,43 +70,6 @@ function AndroidApplicationView(applicationViewModel) {
       previousView.window.show();
     }
   };
-
-  // handle favourites
-  // use action bar when supported - http://jira.appcelerator.org/browse/TIMOB-2371
-  if (Ti.Platform.osname === "android") {
-    Titanium.Android.currentActivity.onCreateOptionsMenu = function (e) {
-      var menu = e.menu;
-      var menuItem = menu.add({
-        title:"Add to Favourites"
-      });
-      menuItem.addEventListener("click", function (e) {
-        var viewModel = applicationViewModel.currentViewModel();
-        viewModel.addToFavourites();
-      });
-      menuItem = menu.add({
-        title:"Remove from Favourites"
-      });
-      menuItem.addEventListener("click", function (e) {
-        var viewModel = applicationViewModel.currentViewModel();
-        viewModel.addToFavourites();
-      });
-      menuItem = menu.add({
-        title:"View Favourites"
-      });
-      menuItem.addEventListener("click", function (e) {
-        applicationViewModel.navigateToFavourites();
-      });
-    };
-
-    Titanium.Android.currentActivity.onPrepareOptionsMenu = function (e) {
-      var menu = e.menu;
-      var viewModel = applicationViewModel.currentViewModel();
-      var isPropertyViewModel = viewModel instanceof PropertyViewModel;
-      menu.items[0].visible = isPropertyViewModel && !viewModel.isFavourite();
-      menu.items[1].visible = isPropertyViewModel && viewModel.isFavourite();
-      menu.items[2].visible = !isPropertyViewModel;
-    };
-  }
 }
 
 module.exports = AndroidApplicationView;
