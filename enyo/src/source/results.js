3c42e1a8eb17c6955f8797575e55cfa739d6808a
Matthew Dunsdon <matthewdunsdon@gmail.com>
2014-04-08 16:22:11 +0100

    Improved look of app in Enyo
    + Reduced height of list items
    + Prevented image from being stretched
    + Simplified some CSS rules

--- enyo/src/source/results.js
+++ enyo/src/source/results.js
@@ -12,17 +12,17 @@ enyo.kind({
 			{kind: "PC.Button", classes:"back-button", ontap: "goBack"},
 			{name: "resultsHeader", content: "X of X matches"}
 		]},
-		{name: "resultsError", kind: "onyx.Drawer", open: false, classes: "panel-row error-drawer", components: [
-			{name: "resultsErrorContent", content: "There was a problem loading the listings."}
-		]},
-		{name: "resultsBox", kind: "onyx.Groupbox", classes: "panel-row", fit: true, layoutKind:"FittableRowsLayout", style: "border: none !important;", components: [
-			{kind: "onyx.GroupboxHeader", content: "Found locations"},
-			{name: "resultsList", kind: "List", fit: true, touch: true, onSetupItem: "setupResultsListItem", components: [
-				{name: "item3", style: "font-size:20px;", classes: "item enyo-border-box", ontap: "resultsListItemTap", components: [
-					{name: "listItemThumb", kind: "enyo.Image", style: "inline-block"},
-					{style: "display:inline-block; margin-left:14px;", components: [
-						{name: "listItemPrice", allowHtml: "true", style: "font-size:26px"},
-						{name: "listItemTitle"}
+		{kind: "FittableRows", name: "resultsContent", classes: "content-panel", fit:true, components: [
+			{kind: "onyx.Drawer", name: "resultsError", open: false, classes: "error-drawer", components: [
+				{name: "resultsErrorContent", content: "There was a problem loading the listings."}
+			]},
+			{kind: "onyx.Groupbox", name: "resultsBox", fit: true, layoutKind:"FittableRowsLayout", style: "border: none !important;", components: [
+				{kind: "onyx.GroupboxHeader", content: "Found locations"},
+				{kind: "List", name: "resultsList", fit: true, touch: true, onSetupItem: "setupResultsListItem", components: [
+					{name: "item3", classes: "list-item-with-image enyo-border-box", ontap: "resultsListItemTap", components: [
+						{kind: "enyo.Image", name: "listItemThumb"},
+						{name: "listItemPrice", classes: "list-item-title", allowHtml: "true"},
+						{name: "listItemTitle", classes: "list-item-subtitle"}
 					]}
 				]}
 			]}
@@ -32,7 +32,7 @@ enyo.kind({
 			{name: "moreButton", kind: "PC.Button", style: "display:block;margin-left:20px;margin-bottom:20px;", showing: false, content: "Load more...", ontap: "getMoreListings"}
 		]},
 
-		{name: "loadingPopup", kind: "messagePopup", message: "Loading..."}
+		{kind: "messagePopup",name: "loadingPopup",  message: "Loading..."}
 	],
 
 	listings: [],
