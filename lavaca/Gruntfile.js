22e089c1e38f74bf53d7f2d5a55bc600cdf177eb
Matthew Dunsdon <matthewdunsdon@gmail.com>
2014-04-29 17:15:50 +0100

    Updated lavaca assets
    Including icons, screenshots and Cordova config files

--- lavaca/Gruntfile.js
+++ lavaca/Gruntfile.js
@@ -409,7 +409,7 @@ module.exports = function( grunt ) {
     initCordova: {
       init: {
         options: {
-          appName: 'PropertyCross',
+          appName: 'PropertyCross - lavaca',
           id: 'com.propertycross.lavaca'
         }
       }
