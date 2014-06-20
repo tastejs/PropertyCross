867d15bcf626954fc7c79d250599a378f6db53e8
Matthew Dunsdon <matthewdunsdon@gmail.com>
2014-06-20 12:15:06 +0100

    Update UI for Property Search page on iOS 7
    ... to make use of storyboard features

--- native/ios/PropertyCross/RecentSearchesTableViewSource.m
+++ native/ios/PropertyCross/RecentSearchesTableViewSource.m
@@ -20,34 +20,18 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell* cell = (UITableViewCell*)[tableView dequeueReusableCellWithIdentifier:@"recentSearchCell"];
-    if (!cell)
-    {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
-                                      reuseIdentifier:@"recentSearchCell"];
-    }
-    
+    UITableViewCell* cell = (UITableViewCell*)[tableView dequeueReusableCellWithIdentifier:@"SearchResultsCell"];
+
     RecentSearchDataEntity* recentSearch = (RecentSearchDataEntity*)self.items[indexPath.row];
+    
     cell.textLabel.text = recentSearch.displayString;
-    
-    // add an extra label to the cell in order to display the number of matches
-    UILabel* leftHandLabel = (UILabel*)[cell viewWithTag:10];
-    if (leftHandLabel == nil)
-    {
-        leftHandLabel = [[UILabel alloc] initWithFrame:CGRectMake(cell.frame.size.width - 100, 0, 80, cell.frame.size.height)];
-        leftHandLabel.backgroundColor = [UIColor clearColor];
-        leftHandLabel.textAlignment = NSTextAlignmentRight;
-        leftHandLabel.tag = 10;
-        [cell addSubview:leftHandLabel];
-    }
-    
-    leftHandLabel.text = [recentSearch.matches stringValue];
+    cell.detailTextLabel.text = [recentSearch.matches stringValue];
     
     return cell;
 }
 
-- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+- (UIView*)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
 {
-    return @"Recent searches:";
+    return (UITableViewCell*)[tableView dequeueReusableCellWithIdentifier:@"RecentSearchesHeaderCell"];
 }
 @end
