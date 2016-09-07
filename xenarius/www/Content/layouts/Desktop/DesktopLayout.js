582fcffe9cde84ab4ec2f8b44635347ff4e9be0c
Dmitry Statsenko <Dem0n13.kaluga@gmail.com>
2016-09-07 12:18:17 +0300

    updated xapp, removed pnonegap project

--- xenarius/www/Content/layouts/Desktop/DesktopLayout.js
+++
@@ -1,8 +0,0 @@
-(function($, DX, undefined) {
-    var layoutSets = DX.framework.html.layoutSets;
-    layoutSets["desktop"] = layoutSets["desktop"] || [];
-    layoutSets["desktop"].push({
-        platform: "generic",
-        controller: new DX.framework.html.DefaultLayoutController({name: "desktop"})
-    })
-})(jQuery, DevExpress);
